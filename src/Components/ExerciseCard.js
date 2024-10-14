aecfb719ce828afbca38f175c01744ce31626f07
Kavyansh <kavyansh14@gmail.com>
2024-10-15 00:45:45 +0530

    Update

--- src/Components/ExerciseCard.js
+++ src/Components/ExerciseCard.js
@@ -7,28 +7,22 @@ const ExerciseCard = ({ exercise }) => {
     <Link 
       className="exercise-card" 
       to={`/exercise/${exercise.id}`} 
-      style={{ 
-        width: '100%',  // Change width to 100% for responsiveness
-        maxWidth: '400px', // Maximum width for larger screens
-        textDecoration: 'none', 
-        margin: '10px',  // Add margin for spacing
-        display: 'block',
-        boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.2)', 
-        borderRadius: '10px', 
-        backgroundColor: '#fff'
+       style={{ 
+           width: '400px', 
+           textDecoration: 'none', 
+           margin: '1px', 
+           display: 'block',
+           boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.2)', 
+           borderRadius: '10px', 
+           overflow: '', 
+           backgroundColor: '#fff'
       }}
     > 
       <img 
         src={exercise.gifUrl} 
         alt={exercise.name} 
         loading="lazy"
-        style={{ 
-          width: '100%',   // Set width to 100% for responsiveness
-          height: 'auto',   // Maintain aspect ratio
-          borderTopLeftRadius: '10px', // Round corners
-          borderTopRightRadius: '10px', // Round corners
-          display: 'block' 
-        }} 
+        style={{ width: '80%', height: 'auto', margin: '0 auto', display: 'block' }} 
       />
 
       <Stack direction="row" spacing={2} sx={{ mt: '10px', justifyContent: 'center' }}>
@@ -45,11 +39,11 @@ const ExerciseCard = ({ exercise }) => {
               background: '#ff7f7f', 
               transform: 'scale(1.05)' 
             }
-          }}>
-          {exercise.bodyPart}
-        </Button>
+        }}>
+        {exercise.bodyPart}
+      </Button>
 
-        <Button 
+      <Button 
           sx={{ 
             ml: '21px',
             color: '#fff', 
@@ -63,19 +57,19 @@ const ExerciseCard = ({ exercise }) => {
               transform: 'scale(1.05)' 
             }
           }}
-        >
-          {exercise.target}
-        </Button>
+      >
+        {exercise.target}
+      </Button>
       </Stack>
 
       <Typography 
-        color='#000' 
-        fontWeight="bold" 
-        mt="11px" 
-        pb="10px" 
-        textTransform='capitalize' 
-        textAlign="center" 
-        fontSize="18px"
+            color='#000' 
+            fontWeight="bold" 
+            mt="11px" 
+            pb="10px" 
+            textTransform='capitalize' 
+            textAlign="center" 
+            fontSize="18px"
       >
         {exercise.name}
       </Typography>
